63f0c0f6fac6db46ebe25b4e4f1da574e0e13364
KimJongMin <whdals011203@naver.com>
2024-09-15 22:43:01 +0900

    refactor: 지원 현황 페이지에 state 필드 추가

--- server/Recruit-Api/src/main/java/com/econovation/recruit/api/applicant/service/ApplicantService.java
+++ server/Recruit-Api/src/main/java/com/econovation/recruit/api/applicant/service/ApplicantService.java
@@ -19,6 +19,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import static com.econovation.recruitcommon.consts.RecruitStatic.PASS_STATE_KEY;
+
 @Service
 @RequiredArgsConstructor
 public class ApplicantService implements ApplicantQueryUseCase {
@@ -46,6 +48,7 @@ public class ApplicantService implements ApplicantQueryUseCase {
     public AnswersResponseDto execute(Integer year, Integer page, String sortType) {
         PageInfo pageInfo = getPageInfo(year, page);
         List<MongoAnswer> result = answerAdaptor.findByYear(year, page);
+        result.forEach(answer -> answer.getQna().put(PASS_STATE_KEY, answer.getApplicantState()));
 
         sortHelper.sort(result, sortType);
         List<Map<String, Object>> sortedResult = result.stream().map(MongoAnswer::getQna).toList();
@@ -104,6 +107,7 @@ public class ApplicantService implements ApplicantQueryUseCase {
     @Override
     public AnswersResponseDto search(Integer page, String searchKeyword) {
         List<MongoAnswer> answers = answerAdaptor.findBySearchKeyword(page, searchKeyword);
+        answers.forEach(answer -> answer.getQna().put(PASS_STATE_KEY, answer.getApplicantState()));
         return AnswersResponseDto.of(
                 answers.stream().map(MongoAnswer::getQna).toList(),
                 new PageInfo(answers.size(), page));
