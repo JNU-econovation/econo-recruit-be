cc294813e709e20efde9a56dcf912f08678ce6bf
Jin Hyuk <ekrrdj07@gmail.com>
2024-09-16 14:35:43 +0900

    [fix]: 지원서 합/불 관리페이지 응답 오류 수정

--- server/Recruit-Api/src/main/java/com/econovation/recruit/api/applicant/dto/GetApplicantsStatusResponse.java
+++ server/Recruit-Api/src/main/java/com/econovation/recruit/api/applicant/dto/GetApplicantsStatusResponse.java
@@ -1,5 +1,7 @@
 package com.econovation.recruit.api.applicant.dto;
 
+import com.econovation.recruitdomain.domains.applicant.domain.state.ApplicantState;
+import com.econovation.recruitdomain.domains.applicant.exception.ApplicantWrongStateException;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -12,23 +14,29 @@ import static com.econovation.recruitcommon.consts.RecruitStatic.PASS_STATE_KEY;
 @AllArgsConstructor
 @Builder
 public class GetApplicantsStatusResponse {
+    private String field;
     private String field1;
     private String field2;
-    private String field3;
     private String name;
     private String id;
     private Integer year;
-    private String state;
+    private ApplicantState state;
 
-    public static GetApplicantsStatusResponse of(Map<String,Object> result) {
+    public static GetApplicantsStatusResponse of(Map<String, Object> result) {
+        ApplicantState state;
+        if (result.get(PASS_STATE_KEY) instanceof ApplicantState applicantState) {
+            state = applicantState;
+        } else {
+            throw ApplicantWrongStateException.wrongStatusException;
+        }
         return GetApplicantsStatusResponse.builder()
+                .field((String) result.get("field"))
                 .field1((String) result.get("field1"))
                 .field2((String) result.get("field2"))
-                .field3((String) result.get("field3"))
                 .name((String) result.get("name"))
                 .id((String) result.get("id"))
                 .year((Integer) result.get("year"))
-                .state(result.get(PASS_STATE_KEY).toString())
+                .state(state)
                 .build();
     }
 }
